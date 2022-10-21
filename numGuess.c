3f0e734e6674237305291f34227e716c35c5c30e
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 15:34:05 -0700

    first commit, comment outline

---
+++ numGuess.c
@@ -0,0 +1,27 @@
+//add headers here
+
+//add main
+
+//display menu
+
+//prompt user for option, 1 to 3
+
+//add game here option 1
+//make random number from 1 to 10
+//prompt user for number
+//tell user if too high or low
+// loop until win or "q"
+
+
+//add option 2, change max number
+//display, tell user max number, no negatives
+//prompt user for number
+//check if number is valid
+//go back to menu
+
+//add option 3, quit
+//display, thank user for playing and end program
+
+//return success
+
+/////////////////******new branch, save max number request for next time?
