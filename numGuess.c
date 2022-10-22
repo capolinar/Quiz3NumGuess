3b34ceeca5691d126c194bf34f784a9f6d21d8b6
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 23:06:05 -0500

    Added while loop, added prints for menu

--- numGuess.c
+++ numGuess.c
@@ -7,28 +7,40 @@
 int main(void)
 {
     //display menu
+    int menu = 0;
+    while(menu != 3)
+    {
+        //prompt user for option, 1 to 3
+        printf("Press 1 to play a game\n");
+        printf("Press 2 to change the max number\n");
+        printf("Press 3 to quit\n");
+        
 
-//prompt user for option, 1 to 3
-
-//add game here option 1
-//make random number from 1 to 10
-//prompt user for number
-//tell user if too high or low
-// loop until win or "q"
+    //add game here option 1
+    //make random number from 1 to 10
+    //prompt user for number
+    //tell user if too high or low
+    // loop until win or "q"
 
 
-//add option 2, change max number
-//display, tell user max number, no negatives
-//prompt user for number
-//check if number is valid
-//go back to menu
+    //add option 2, change max number
+    //display, tell user max number, no negatives
+    //prompt user for number
+    //check if number is valid
+    //go back to menu
 
-//add option 3, quit
-//display, thank user for playing and end program
+    //add option 3, quit
+    //display, thank user for playing and end program
 
-//return success
+    }
+    
 
-/////////////////******new branch, save max number request for next time?
+
+    
+
+    //return success
+
+    /////////////////******new branch, save max number request for next time?
 
     return EXIT_SUCCESS;
 
